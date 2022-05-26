1592b4031e6e3ec21cf6a6a52295e789e12e060c
AngelDiaaz <93326288+AngelDiaaz@users.noreply.github.com>
2022-05-26 12:35:42 +0200

    Registro hecho

--- puertamagicafe/js/login.js
+++ puertamagicafe/js/login.js
@@ -1,28 +1,108 @@
-$(document).ready(function(){
-    $("#btn-login").click(function(){
-        let username = $("#email").val().trim();
-        let password = $("#pwd").val().trim();
+// $(document).ready(function(){
+//     $("#btn-login").click(function(){
+//         let username = $("#email").val().trim();
+//         let password = $("#pwd").val().trim();
 
-        console.log(username)
+//         console.log(username)
         
 
-        if( username != "" && password != "" ){
-            $.ajax({
-                url:'https://localhost:5001/Login',
-                type:'post',
-                data:JSON.stringify({Name:username,Password:password}),
-                contentType:"application/json; charset=utf-8",
-                success:function(response){
-                    if(response){
-                        window.location = "welcome.html";
-                    } else {
-                        alert('Error en el login');
-                    }
-                },
-                error:function(){
-                    $("#error").fadeIn();
+//         if( username != "" && password != "" ){
+//             $.ajax({
+//                 url:'https://localhost:5001/Login',
+//                 type:'post',
+//                 data:JSON.stringify({Name:username,Password:password}),
+//                 contentType:"application/json; charset=utf-8",
+//                 success:function(response){
+//                     if(response){
+//                         window.location = "welcome.html";
+//                     } else {
+//                         alert('Error en el login');
+//                     }
+//                 },
+//                 error:function(){
+//                     $("#error").fadeIn();
+//                 }
+//             });
+//         }
+//     });
+// });
+
+function login(){
+    let username = $("#email").val().trim();
+    let password = $("#pwd").val().trim();
+
+    console.log(username)
+    
+
+    if( username != "" && password != "" ){
+        $.ajax({
+            url:'https://localhost:5001/Login',
+            type:'post',
+            data:JSON.stringify({Name:username,Password:password}),
+            contentType:"application/json; charset=utf-8",
+            success:function(response){
+                if(response){
+                    window.location = "welcome.html";
+                } else {
+                    alert('Error en el login');
                 }
-            });
+            },
+            error:function(){
+                $("#error").fadeIn();
+            }
+        });
+    }
+}
+
+function goregister(){
+    window.location = "register.html";
+}
+
+function back(){
+    window.location = "index.html";
+}
+
+function register(){
+    let username = $("#user").val().trim();
+    let email = $("#email").val().trim();
+    let password = $("#pwd").val().trim();
+    let rpassword = $("#re-pwd").val().trim();
+    let date = $("#date").val().trim();
+
+    console.log(username)
+    console.log(email)
+    console.log(password)
+    console.log(rpassword)
+    console.log(date)
+    
+    
+
+    if( username != "" && password != "" && rpassword != "" && email != "" && date != ""){
+        if(password == rpassword){
+        $.ajax({
+            url:'https://localhost:5001/User/Register',
+            type:'post',
+            data:JSON.stringify({Name:username,Password:password,Email:email,Date:date}),
+            contentType:"application/json; charset=utf-8",
+            success:function(response){
+                window.location = "index.html";
+            },
+            error:function(){
+                $("#error").fadeIn();
+            }
+        });
+        } else {
+            alert('Las contraseñas no son iguales')
         }
-    });
-});
+    } else {
+        alert('Rellene todos los campos por favor')
+    }
+}
+
+// $(document).ready(function(){
+//     $("#btn-register").click(function(){
+        
+//         window.location = "register.html";
+                    
+//     });
+// });
