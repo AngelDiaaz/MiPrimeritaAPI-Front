60c7620da6555b0970507bef89240f4389db1f58
AngelDiaaz <93326288+AngelDiaaz@users.noreply.github.com>
2022-05-27 17:52:20 +0200

    Extras hecho

---
+++ puertamagicafe/js/update.js
@@ -0,0 +1,30 @@
+function back(){
+    window.location = "welcome.html";
+}
+
+function update(){
+    let name = $("#name").val().trim();
+    let dni = $("#dni").val().trim();
+    let pin = $("#pin").val().trim();
+
+    console.log(name)
+    console.log(dni)
+    console.log(pin)
+
+    if( name != "" && dni != "" && pin != "" ){
+        $.ajax({
+            url:'https://localhost:5001/Alumno/Update',
+            type:'put',
+            data:JSON.stringify({Nombre:name,DNI:dni,PIN:pin}),
+            contentType:"application/json; charset=utf-8",
+            success:function(response){
+                window.location = "welcome.html";
+            },
+            error:function(){
+                $("#error").fadeIn();
+            }
+        });
+    } else {
+        alert('Rellene todos los campos por favor');
+    }
+}
